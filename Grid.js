fdeca3552b1e1399ebc4c1795b6717f0d085490f
0maumau0 <strubemaurice@gmail.com>
2026-05-31 22:21:30 +0200

    fixed scaling bug by calculating rows and adjust height claculation

--- Grid.js
+++ Grid.js
@@ -3,13 +3,14 @@ var Grid;
 (function (Grid) {
     const containerGrid = document.getElementById("containerGrid");
     const imageCount = document.getElementById("containerGrid")?.childElementCount;
-    const columsAndRows = Math.floor(Math.sqrt(imageCount)) + 1;
-    const rowsHeight = containerGrid.offsetHeight / columsAndRows;
-    const columsWidth = containerGrid.offsetWidth / columsAndRows;
+    const columsAndRows = Math.floor(Math.sqrt(imageCount));
+    const rowsCount = Math.ceil(imageCount / columsAndRows);
+    const rowsHeight = containerGrid.offsetHeight / rowsCount;
+    let border = 10;
+    const columsWidth = (containerGrid.offsetWidth / columsAndRows) - border;
     function setupGrid() {
-        let border = 10;
         containerGrid.style.display = "grid";
-        containerGrid.style.gridTemplateColumns = "repeat(" + columsAndRows + "," + (columsWidth - border) + "px)";
+        containerGrid.style.gridTemplateColumns = "repeat(" + columsAndRows + "," + columsWidth + "px)";
         containerGrid.style.gridTemplateRows = "auto";
         containerGrid.style.gap = "5px";
         containerGrid.style.alignItems = "center";
@@ -19,10 +20,8 @@ var Grid;
     function setImageHeightandWidth() {
         for (let i = 1; i <= imageCount; i++) {
             let image = document.getElementById("image-" + i + "");
-            if (image === null)
-                return;
-            image.height = rowsHeight;
-            image.width = columsWidth;
+            image.style.maxHeight = "" + rowsHeight + "px";
+            image.style.maxWidth = "" + columsWidth + "px";
         }
     }
     setupGrid();
--- Grid.ts
+++ Grid.ts
@@ -2,15 +2,16 @@ namespace Grid {
 
     const containerGrid: HTMLDivElement = document.getElementById("containerGrid") as HTMLDivElement;
     const imageCount: number = document.getElementById("containerGrid")?.childElementCount!;
-    const columsAndRows: number = Math.floor(Math.sqrt(imageCount)) + 1;
-    const rowsHeight = containerGrid.offsetHeight / columsAndRows;
-    const columsWidth = containerGrid.offsetWidth / columsAndRows;
+    const columsAndRows: number = Math.floor(Math.sqrt(imageCount));
+    const rowsCount: number = Math.ceil(imageCount / columsAndRows)
+    const rowsHeight = containerGrid.offsetHeight / rowsCount;
+    let border: number = 10
+    const columsWidth = (containerGrid.offsetWidth / columsAndRows) - border;
 
 
     function setupGrid(): void {
-        let border: number = 10
         containerGrid.style.display = "grid";
-        containerGrid.style.gridTemplateColumns = "repeat(" + columsAndRows + "," + (columsWidth - border) + "px)"
+        containerGrid.style.gridTemplateColumns = "repeat(" + columsAndRows + "," + columsWidth + "px)"
         containerGrid.style.gridTemplateRows = "auto"
         containerGrid.style.gap = "5px"
         containerGrid.style.alignItems = "center"
@@ -18,19 +19,16 @@ namespace Grid {
         setImageHeightandWidth()
     }
 
-    function setImageHeightandWidth() {
+    function setImageHeightandWidth(): void {
 
         for (let i = 1; i <= imageCount; i++) {
             let image: HTMLImageElement = document.getElementById("image-" + i + "") as HTMLImageElement;
-            if (image === null) return;
-            image.height = rowsHeight;
-            image.width = columsWidth;
+            image.style.maxHeight = "" + rowsHeight + "px";
+            image.style.maxWidth = "" + columsWidth + "px";
 
         }
 
     }
 
-
     setupGrid();
-
 }
